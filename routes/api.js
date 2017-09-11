c123f5d6bb8477b355ff336c5e4b21d6362d7604
Nancy <belscher@nancys-mbp.attlocal.net>
2017-09-10 20:47:46 -0400

    trying to get last two working but they are not yet

--- routes/api.js
+++ routes/api.js
@@ -7,14 +7,15 @@ const mongoose = require('mongoose');
 router.use(bodyParser.json());
 
 const Act = require("../models/act");
-// const Act = models.act;
 
+// return a single activity by id
 router.get('/activities/:id', function(req, res) {
   Act.find({_id: req.params.id}).then(function(results) {
     return res.json(results);
   });
 });
 
+// update a single entry
 router.put('/activities/:id', function(req, res) {
   Act.findOneAndUpdate(
     {_id: req.params.id},
@@ -26,6 +27,29 @@ router.put('/activities/:id', function(req, res) {
     });
 });
 
+// Add tracked data for a day. The data sent with this should include the day tracked.
+// You can also override the data for a day already recorded.
+router.post('/activities/:id/stats', function(req, res) {
+  // I think my data is maybe structured wrong because I don't really see how this is
+  // different from my regular post...
+});
+
+// Remove tracked data for a day.
+router.delete('/stats/:id', function(req, res) {
+  // trying to pull by id, find the date for that entry and then
+  // delete everything with the same date. It doesn't work
+  Act.findOne({id: req.params.id})
+  .then(function(results) {
+    let tempdate = this.date;
+    console.log(tempdate);
+  });
+  Act.delete({date: tempdate})
+  .then(function(results) {
+    return res.json(results);
+  });
+});
+
+// remove an activity by id
 router.delete('/activities/:id', function(req, res) {
   Act.deleteOne({_id: req.params.id})
       .then(function(results) {
@@ -33,12 +57,14 @@ router.delete('/activities/:id', function(req, res) {
     });
 });
 
-
+// get all activities
 router.get('/activities', function(req, res) {
     Act.find().then(function(results) {
       return res.json(results);
     });
 });
+
+// create a new activity entry
 router.post('/activities', function(req, res) {
   Act.create(req.body).then(function(results) {
     return res.json(results);
